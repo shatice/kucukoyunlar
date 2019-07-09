c234e9feb0f6503429f88be7a49ce40ebc67eaa7
shatice <sarah.gungor@hetic.net>
2019-07-09 23:39:54 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// Inspired by yamsellem's hanoï towers
+
+let uls = document.querySelectorAll('ul'), 
+memo;
+
+for (let i = 0; i < uls.length; i++) {
+    uls[i].addEventListener('click', () => {
+        let ul = uls[i], 
+            firstLi = ul.querySelector('li');
+            
+        if (memo && (!firstLi || firstLi.dataset.weight > memo.dataset.weight)) {
+            ul.prepend(memo);
+            memo = undefined;
+
+            if (ul === uls[2] && ul.querySelectorAll('li').length === 2) {
+            ul.classList.add('is-won');
+            } 
+        } else {
+            memo = firstLi; 
+        }
+    }) 
+}
